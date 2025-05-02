5f7228fe795340abc92062afb39fac0db073103d
Yasha-Ehtesham <yasha.ehtesham02@gmail.com>
2025-05-03 03:57:41 +0600

    api-keys-protected

--- thrift/settings.py
+++ thrift/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
+GOOGLE_BOOKS_API_KEY = config('GOOGLE_BOOKS_API_KEY')
+NYT_API_KEY = config('NYT_API_KEY')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
